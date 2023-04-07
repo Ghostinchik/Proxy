7dc0cbb2b2560a3c157a26d9e12f3b078dbf7d77
Sasha <117905137+Ghostinchik@users.noreply.github.com>
2023-04-07 15:14:32 +0300

    Proxy

---
+++ Proxy/Proxy/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Proxy.Proxy;
+
+namespace Proxy
+{
+    public partial class Form1 : Form
+    {
+
+        private List<User> users = new List<User>();
+        private FunctionalityProxy Functionality;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+
+            User user = new User(username, password);
+            users.Add(user);
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+
+            User user = users.FirstOrDefault(u => u.Username == username && u.Password == password);
+
+            if (user != null)
+            {
+                user.IsAuthenticated = true;
+
+                Functionality = new FunctionalityProxy(user);
+                Functionality.PerformFunctionality();
+                MessageBox.Show("Авторизація успішна");
+            }
+            else
+            {
+                MessageBox.Show("Неправильне ім'я користувача або пароль");
+            }
+        }
+    }
+}
---
+++ Proxy/Proxy/Proxy.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proxy
+{
+    internal class Proxy
+    {
+        public interface IFunctionality
+        {
+            void PerformFunctionality();
+        }
+
+        public class Functionality : IFunctionality
+        {
+            public void PerformFunctionality()
+            {
+                MessageBox.Show("Функціонал виконано успішно");
+            }
+        }
+
+        public class User
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public bool IsAuthenticated { get; set; }
+
+            public User(string username, string password)
+            {
+                Username = username;
+                Password = password;
+                IsAuthenticated = false;
+            }
+
+            public bool Authenticate()
+            {
+                return true;
+            }
+        }
+
+        public class FunctionalityProxy : IFunctionality
+        {
+            private User user;
+            private Functionality functionality;
+
+            public FunctionalityProxy(User user)
+            {
+                this.user = user;
+                functionality = new Functionality();
+            }
+
+            public void PerformFunctionality()
+            {
+                if (user.IsAuthenticated)
+                {
+                    functionality.PerformFunctionality();
+                }
+                else
+                {
+                    MessageBox.Show("Доступ заборонено");
+                }
+            }
+        }
+
+    }
+}
